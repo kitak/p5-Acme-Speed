abaf9109f271b1cca8254c35fc1aa60775fc8bf5
Keisuke KITA <kei.kita2501@gmail.com>
2013-10-13 15:53:54 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::Speed - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Speed;
+
+# DESCRIPTION
+
+Acme::Speed is ...
+
+# LICENSE
+
+Copyright (C) Keisuke KITA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Keisuke KITA <kei.kita2501@gmail.com>
---
+++ lib/Acme/Speed.pm
@@ -0,0 +1,39 @@
+package Acme::Speed;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Speed - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Speed;
+
+=head1 DESCRIPTION
+
+Acme::Speed is ...
+
+=head1 LICENSE
+
+Copyright (C) Keisuke KITA.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Keisuke KITA E<lt>kei.kita2501@gmail.comE<gt>
+
+=cut
+
